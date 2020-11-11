4f6337518d9637cc88ddf7201d48bb6645ae0f67
Timos Valais <73531450+TimosValais@users.noreply.github.com>
2020-11-11 15:13:18 +0200

    Add project files.

---
+++ DelegatesExercise/HomeClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExercise
+{
+    class HomeClass
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public HomeClass()
+        {
+            this.Name = "Home";
+            this.Address = "Address";
+        }
+
+
+    }
+}
---
+++ DelegatesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeClass home = new HomeClass();
+            PrintHome print = i => Console.WriteLine(i.Name + " " + i.Address);
+            print(home);
+        }
+
+        public delegate void PrintHome(HomeClass home);
+    }
+}
